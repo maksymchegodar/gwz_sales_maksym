8fde7d2e6a33870084a53002a3a7f256613379ae
Dharmendra Rathod <39919825+rathoddharmendra@users.noreply.github.com>
2025-06-30 17:09:00 +0200

    Revert "Margin"

--- sales_daily.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  date_date,
-  ROUND(SUM(turnover), 2) AS daily_turnover,
-  ROUND(SUM(turnover - purchase_cost), 2) AS daily_margin
-FROM `course17.gwz_sales_17`
-GROUP BY date_date
-ORDER BY date_date;
